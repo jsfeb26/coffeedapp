ee9b2580e7625ea5168f92dc15a0664e863cf695
Jason Stinson <jsfeb26@gmail.com>
2015-04-15 20:59:04 -0400

    Add Additional Views

--- core/urls.py
+++ core/urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
     url(r'^$', coreviews.LandingView.as_view()),
     url(r'location/$', coreviews.LocationListView.as_view()),
     url(r'location/(?P<pk>\d+)/detail/$', coreviews.LocationDetailView.as_view(), name='location_list'),
+    url(r'location/create/$', coreviews.LocationCreateView.as_view()),
+    url(r'search/$', coreviews.SearchListView.as_view()),
+    url(r'location/(?P<pk>\d+)/update/$', coreviews.LocationUpdateView.as_view(), name='location_update'),
+    url(r'location/(?P<pk>\d+)/review/create/$', coreviews.ReviewCreateView.as_view(), name='reveiw_create'),
+    url(r'location/(?P<pk>\d+)/review/udpate/$', coreviews.ReviewUpdateView.as_view(), name='reveiw_update'),
+    url(r'entrance/$', coreviews.entrance),
 ]
